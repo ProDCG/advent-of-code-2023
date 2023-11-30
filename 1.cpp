84b58eeb56150e7331ba06b233f91d1d19a5dc08
Mason Stuart <mason.stuart05@outlook.com>
2023-11-30 14:54:18 -0800

    Template commit

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    string line;
+
+    while (getline(cin, line)) {
+        if (line == "a") break;
+    }
+    
+    return 0;
+}
